99a9404ae1940cb357c4680765fc95e00422f1f3
CelestalKitsune <30158363+CelestialKitsune@users.noreply.github.com>
2025-05-09 15:22:07 +0200

    Shrinkage removal complete

---
+++ src/EnTest4_ShrinkSkip.c
@@ -0,0 +1,8 @@
+#include "modding.h"
+#include "global.h"
+
+RECOMP_HOOK_RETURN("EnTest4_GetBellTimeAndShrinkScreenBeforeDay3")
+void EnTest4_GetBellTimeAndShrinkScreenBeforeDay3(void) {
+  gSaveContext.screenScale = 1000.0f - 200.f;
+  gSaveContext.screenScaleFlag = true;
+}
